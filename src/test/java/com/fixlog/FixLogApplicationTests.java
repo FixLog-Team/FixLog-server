2e15f2d06c32b81749de008f45df6045022d001e
hyeonseo <hyeonseo@nethru.co.kr>
2026-01-05 20:04:09 +0900

    초기 프로젝트 구성

---
+++ src/test/java/com/fixlog/FixLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
