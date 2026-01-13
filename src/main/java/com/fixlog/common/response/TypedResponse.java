54b50256e2872bfed0f86fec9e0bb9dde24ae859
hyeonseo <hyeonseo@nethru.co.kr>
2026-01-14 01:41:34 +0900

    [KAN-11] [feat] 테스트 컨트롤러 생성

---
+++ src/main/java/com/fixlog/common/response/TypedResponse.java
@@ -0,0 +1,36 @@
+package com.fixlog.common.response;
+
+import com.fixlog.common.code.Code;
+
+public abstract class TypedResponse<T> extends Response {
+	private T result;
+
+	public TypedResponse() {
+	}
+
+	public TypedResponse(Code code, String message) {
+		super(code, message);
+	}
+
+	public TypedResponse(Code code, String message, T result) {
+		super(code, message);
+		this.result = result;
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+	public void setResult(T result) {
+		this.result = result;
+	}
+
+	@Override
+	public String toString() {
+		return "TypedResponse{" +
+			"result=" + result +
+			", code=" + code +
+			", message='" + message + '\'' +
+			'}';
+	}
+}
